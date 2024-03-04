8589e0e0548f1a822115b14504f16a0e751d3f0e
thumberlys10 <108513235+thumbs123@users.noreply.github.com>
2024-03-04 15:10:25 +0700

    push

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+class PostController extends Controller
+{
+    public function index()
+    {
+        return view('posts',[
+            "title" => "All Posts",
+            "active" =>'posts',
+            //"posts" => Post::all()
+            "posts" => Post::latest()->get()
+        ]);
+    }
+
+    public function show(Post $post)
+    {
+        return view('post',[
+            "title" => "Single Post",
+            "active" =>'posts',
+            "post" => $post
+        ]);
+    }
+}
---
+++ database/migrations/2024_02_24_105748_create_posts_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('excerpt');
+            $table->text('body');
+            $table->foreignId('category_id');
+            $table->foreignId('user_id');
+            $table->timestamp('published_at')->nullable(); 
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('post');
+    }
+};
